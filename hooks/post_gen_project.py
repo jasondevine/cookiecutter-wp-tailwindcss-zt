8f00f938587837ac2c705458a09372b5f0599f2d
jasondevine <jsndvn@gmail.com>
2020-11-23 19:54:37 +0000

    Bug: solving syntax error from original python file in cookicutter template that this is based on (github.com/ggarcia24/cookiecutter-wptheme)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,4 @@ import os
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
-if __name__ == '__main__':
-    print 'Please, run the command "npm install" and after that "grunt init" on the directory {dir}'.format(
-        dir=PROJECT_DIRECTORY
-    )
+print("Hello, {{cookiecutter.author_name}}!")
